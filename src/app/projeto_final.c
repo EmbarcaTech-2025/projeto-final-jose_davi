64691aa33737fcbb26218b167afaa01828bb9439
DaviHVL <davihvl@gmail.com>
2025-08-04 15:57:18 -0300

    feat: adição do módulo de conectividade wi-fi com o protocolo mqtt

--- src/app/projeto_final.c
+++ src/app/projeto_final.c
@@ -1,4 +1,6 @@
 #include "keyboard_handler.h"
+#include "../include/mqtt_comm.h"
+#include "../include/wifi_conn.h"
 #include "pico/stdio.h"
 #include "pico/time.h"
 #include <stdio.h>
@@ -7,6 +9,9 @@ int main() {
   stdio_init_all();
 
   keyboard_init();
+
+  // connect_to_wifi("Nome da Rede", "Senha da Rede");
+  // mqtt_setup("bitdog1", "IP do Broker", "user", "password");
 
   while (true) {
     char key = get_key();
