a83d5d85cfbc7f3c2a1392b982198fc189e14f0f
José Moraes <joseaugustomoraes@protonmail.com>
2025-08-29 12:41:13 -0300

    refactor: correção de bugs nas função de salvamento de arquivos e de envio de mensagens I2C

--- src/hal/i2c_messages.h
+++ src/hal/i2c_messages.h
@@ -4,15 +4,15 @@
 #include "sd_card_handler.h"
 
 static const uint SLAVE_ADDRESS = 0x26;
-static const uint I2C_BAUDRATE = 10 * 1000;
+static const uint I2C_BAUDRATE = 100 * 1000;
 
-#define SLAVE_PORT i2c0
-#define MASTER_PORT i2c1
+#define SLAVE_PORT i2c1
+#define MASTER_PORT i2c0
 
-#define I2C_SLAVE_SDA_PIN 0
-#define I2C_SLAVE_SCL_PIN 1
-#define I2C_MASTER_SDA_PIN 2
-#define I2C_MASTER_SCL_PIN 3
+#define I2C_SLAVE_SDA_PIN 2
+#define I2C_SLAVE_SCL_PIN 3
+#define I2C_MASTER_SDA_PIN 0
+#define I2C_MASTER_SCL_PIN 1
 
 typedef enum {
   READING,
